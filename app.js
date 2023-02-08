a4f9e34df3cb376d1660c91dc2cc102c708806f7
Sreedevi Ravindranadhan <sreedeviravindranadhan@gmail.com>
2023-02-08 09:14:45 -0600

    added favorite router

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ connect.then(() => console.log('Connected correctly to server'),
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -41,12 +51,12 @@ app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/categories', categoryRouter);
-app.use('/recipes', recipeRouter);
-app.use('/ingredients', ingredientRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/categories', categoryRouter);
+app.use('/recipes', recipeRouter);
+app.use('/ingredients', ingredientRouter);
 app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
